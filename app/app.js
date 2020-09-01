04fd1362fa9f25cdb154dc310cb346ad8f640220
GoffetteDorian <goffettedorian@gmail.com>
2020-09-01 14:25:20 +0200

    Cleaning project folder

--- app/app.js
+++ app/app.js
@@ -1,18 +1,8 @@
-import Vue from 'nativescript-vue';
-import {
-  ModalStack,
-  overrideModalViewMethod,
-  VueWindowedModal,
-} from 'nativescript-windowed-modal';
-
-import Home from './components/Home';
+import Vue from "nativescript-vue";
+import Home from "./components/Home";
 
 Vue.config.silent = false;
-overrideModalViewMethod();
-
-Vue.registerElement('ModalStack', () => ModalStack);
-Vue.use(VueWindowedModal);
 
 new Vue({
-  render: (h) => h('frame', [h(Home)]),
+  render: h => h("frame", [h(Home)])
 }).$start();
