111d6ca803e1dc135e62c6b470a8d4bd05c85704
GoffetteDorian <goffettedorian@gmail.com>
2020-09-01 16:42:22 +0200

    Basic login screen mockup

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,20 +1,30 @@
 <template>
   <Page class="page" actionBarHidden="true">
-   <Label text="Hello world" />
+    <FlexboxLayout class="page">
+      <StackLayout class="form">
+        <Image class="logo" src="~/assets/logo.png" />
+        <StackLayout class="input-field">
+          <TextField class="input" />
+        </StackLayout>
+
+        <StackLayout class="input-field">
+          <TextField class="input" />
+        </StackLayout>
+
+        <Button text="Log In" class="btn btn-primary" />
+      </StackLayout>
+
+      <Label text="Don't have an account?" />
+    </FlexboxLayout>
   </Page>
 </template>
 
 <script>
-
 export default {
   data() {
-    return {
-      
-    };
-  },
+    return {};
+  }
 };
 </script>
 
-<style scoped lang="css">
-
-</style>
+<style scoped lang="css"></style>
