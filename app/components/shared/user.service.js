8addc0de09a81c4d0addb3129711ce2f1dad992e
GoffetteDorian <goffettedorian@gmail.com>
2020-09-09 12:04:14 +0200

    turning es5 to esnext

--- app/components/shared/user.service.js
+++ app/components/shared/user.service.js
@@ -1,8 +1,8 @@
 import { Kinvey } from "kinvey-nativescript-sdk";
 import { User } from "./user.model";
 
-export const UserService = () => {
-  function register(user) {
+export class UserService {
+  register(user) {
     return new Promise((resolve, reject) => {
       Kinvey.User.logout().then(() => {
         Kinvey.User.signup({ username: user.email, password: user.password })
@@ -15,7 +15,7 @@ export const UserService = () => {
     });
   }
 
-  function login(user) {
+  login(user) {
     return new Promise((resolve, reject) => {
       Kinvey.User.logout().then(() => {
         Kinvey.User.login(user.email, user.password)
@@ -28,11 +28,11 @@ export const UserService = () => {
     });
   }
 
-  function resetPassword(email) {
+  resetPassword(email) {
     return Kinvey.User.resetPassword(email).catch(this.handleErrors);
   }
 
-  function handleErrors(error) {
+  handleErrors(error) {
     console.error(error.message);
   }
-};
+}
