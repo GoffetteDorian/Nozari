f8fe92de0f2a4f65b6de7ee0562262c8a7143d32
GoffetteDorian <goffettedorian@gmail.com>
2020-09-09 12:03:00 +0200

    Splitting login and homepage, now homepage will be shown after the login

---
+++ app/components/Home/HomePage.vue
@@ -0,0 +1,47 @@
+<template>
+  <Page actionBarHidden="true" backgroundSpanUnderStatusBar="true">
+    <StackLayout class="page">
+      <Image class="logo" src="~/assets/logo.png" />
+
+      <GridLayout columns="*, *" rows="*, *">
+        <Button class="btn" row="0" col="0" text="Events" @tap="onEventTap" />
+        <Button class="btn" row="0" col="1" text="Armory" @tap="onArmoryTap" />
+        <Button
+          class="btn"
+          row="1"
+          col="0"
+          colSpan="2"
+          text="Calendar"
+          @tap="onCalendarTap"
+        />
+      </GridLayout>
+    </StackLayout>
+  </Page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    onEventTap() {
+      console.log("Events tapped");
+    },
+
+    onArmoryTap() {
+      console.log("Armory tapped");
+    },
+
+    onCalendarTap() {
+      console.log("Calendar tapped");
+    }
+  }
+};
+</script>
+
+<style scoped>
+button {
+  background-color: rgba(179, 61, 38, 0.5);
+}
+</style>
